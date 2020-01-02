f462b9b5f47bf35ffa682343fa6df86f4328c639
ilad <uaremycaptain@gmail.com>
2020-01-02 10:54:54 +0800

    'secondVersion'

--- djangoTest5/urls.py
+++ djangoTest5/urls.py
@@ -19,6 +19,6 @@ from firstApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home),
+    path('', views.home),
     path('subhome/', views.sub_home),
 ]
