f9dc6e389e7a1ae7a85e255c0e53758a6ffe22fc
ilad <uaremycaptain@gmail.com>
2020-01-02 11:37:38 +0800

    set host in linux

--- djangoTest5/settings.py
+++ djangoTest5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+q*rc^iuzoa(zfymt(yp^^zp-89=kk(r^8*yr68b0qhcjbf&64'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['#']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
